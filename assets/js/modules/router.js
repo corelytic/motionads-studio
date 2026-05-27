ba6fd399b9e0cab7b565ae5d3c1c84ed87a5114c
corelytic <omnilabs76@gmail.com>
2026-05-27 22:13:32 +0300

    Delete assets directory

--- assets/js/modules/router.js
+++
@@ -1,9 +0,0 @@
-function createRouter(onRouteChange) {
-  const getRoute = () => (location.hash.replace("#", "") || "dashboard");
-  const handle = () => onRouteChange(getRoute());
-  window.addEventListener("hashchange", handle);
-  handle();
-  return { current: getRoute, dispose() { window.removeEventListener("hashchange", handle); } };
-}
-
-window.CorelyticRouter = { createRouter };
